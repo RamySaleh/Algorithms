54929ed6ed27b7916bdbaecb2e0cdec4caf94b04
ramy.saleh <ramy.saleh.me@gmail.com>
2020-01-15 20:37:36 +0000

    permutations

--- Interviews_LNK/permutations.py
+++ Interviews_LNK/permutations.py
@@ -17,14 +17,20 @@ class Solution(test_class.test_class):
     def dfs(self, nums, path, res):
         if not nums:
             res.append(path)
+            return
 
         for i in range(len(nums)):
             self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
 
-    def permute_a7a(self, nums):
-        return [[n] + p
-                for i, n in enumerate(nums)
-                for p in self.permute(nums[:i] + nums[i + 1:])] or [[]]
+    def permute_itr(self, nums):
+        perms = [[]]
+        for n in nums:
+            new_perms = []
+            for perm in perms:
+                for i in range(len(perm) + 1):
+                    new_perms.append(perm[:i] + [n] + perm[i:])  ###insert n
+            perms = new_perms
+        return perms
 
     def test_1(self):
         self.assertEqual([[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]], self.permute([1,2,3]))
