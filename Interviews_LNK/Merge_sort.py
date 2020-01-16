a648079d8eb0245934f8315eb83160f7ccb92742
ramy.saleh <ramy.saleh.me@gmail.com>
2020-01-16 16:06:00 +0000

    merge sort

---
+++ Interviews_LNK/Merge_sort.py
@@ -0,0 +1,15 @@
+from Helpers import helper as hlp
+from Helpers import test_class
+import re
+
+class Solution(test_class.test_class):
+
+    def setUp(self):
+        super().setUp()
+
+    def mergeSort(self, nums):
+        res = []
+        return res
+
+    def test_1(self):
+        self.assertEqual([1,2,3,4,7,9], self.mergeSort([7,3,9,1,2,4]))
