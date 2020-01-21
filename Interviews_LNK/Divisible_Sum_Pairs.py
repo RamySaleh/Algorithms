33c914ce8e34c6bba7b4b87924fda95defb4f2dd
ramy.saleh <ramy.saleh.me@gmail.com>
2020-01-21 21:35:04 +0000

    sum pairs

--- Interviews_LNK/Divisible_Sum_Pairs.py
+++ Interviews_LNK/Divisible_Sum_Pairs.py
@@ -9,27 +9,26 @@ class Solution(test_class.test_class):
         super().setUp()
 
     def divisibleSumPairs(self, n, k, ar):
-        lookup = {}
-        mod_arr = []
-        i = 0
-        for num in ar:
-            if num not in lookup:
-                lookup[num] = [i]
+        mods = [0] * k
+        for i in range(len(ar)):
+            mods[ar[i] % k] += 1
+
+        count = 0
+        for i in range(int(k / 2) + 1):
+            if i == 0:
+                count += int(mods[i] * (mods[i] - 1) / 2) # for 0 the number of pairs : n * (n - 1) / 2
+            elif float(i) == (k / 2):
+                count += int(mods[int(i)] * (mods[int(i)] - 1) / 2) # if k is even then the item in the middle has no pairs with any other mods
             else:
-                lookup[num].append(i)
-            i += 1
-            mod_arr.append(num % k)
+                count += int(mods[i] * mods[k - i]) # for adj pairs : n * m
 
-        counter = 0
-        res = {}
-        for num in mod_arr:
-            adj = k - num
-            if adj in lookup:
-                counter += len(lookup[adj])
-
-        return counter
-
-
+        return count
 
     def test_1(self):
         self.assertEqual(5, self.divisibleSumPairs(0,3,[1,3,2,6,1,2]))
+
+    def test_2(self):
+        self.assertEqual(15, self.divisibleSumPairs(0,3,[29,97,52,86,27,89,77,19,99,96]))
+
+    def test_3(self):
+        self.assertEqual(4, self.divisibleSumPairs(0,2,[5,9,10,7,4]))
