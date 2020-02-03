e819522a7caf4cc35b407a36f558d352f20f3721
ramy.saleh <ramy.saleh.me@gmail.com>
2020-02-03 22:17:23 +0000

    palindrom find-bottom-left-tree-value

--- Binary_Tree/Binary_tree_identical.py
+++ Binary_Tree/Binary_tree_identical.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/same-tree/
+#https://leetcode.com/problems/find-bottom-left-tree-value/
 from typing import List
 
 from Helpers import helper as hlp
@@ -10,50 +10,25 @@ class Solution(test_class.test_class):
     def setUp(self):
         super().setUp()
 
-    def isIdentical_itr(self, root1, root2) -> List[List[int]]:
-        stack1 = [root1]
-        stack2 = [root2]
+    def findBottomLeftValue(self, root) -> int:
+        stack = [[root]]
+        nodes = []
+        while stack:
+            children = []
+            nodes = stack.pop()
+            for node in nodes:
+                if node:
+                    if node.left:
+                        children.append((node.left))
+                    if node.right:
+                        children.append((node.right))
+            if children:
+                stack.append(children)
 
-        while stack1 and stack2:
-            curr1 = stack1.pop()
-            curr2 = stack2.pop()
-
-            if not curr1 and not curr2:
-                continue
-
-            if (curr1 and not curr2) or (curr2 and not curr1) or (curr1.val != curr2.val):
-                return False
-
-            stack1.append(curr1.left)
-            stack1.append(curr1.right)
-
-            stack2.append(curr2.left)
-            stack2.append(curr2.right)
-        return True
-
-    def isIdentical(self, root1, root2) -> List[List[int]]:
-        return self.dfs(root1, root2)
-
-    def dfs(self, root1, root2) -> List[List[int]]:
-        if not root1 and not root2:
-            return True
-
-        if not root1 or not root2 or root1.val != root2.val:
-            return False
-
-        return self.dfs(root1.left, root2.left) and self.dfs(root1.right, root2.right)
+        return nodes[0].val
 
     def test_1(self):
-        self.assertEqual(True, self.isIdentical(self.build_tree1(),self.build_tree1()))
-
-    def test_2(self):
-        self.assertEqual(False, self.isIdentical(self.build_tree1(),self.build_tree2()))
-
-    def test_1_1(self):
-        self.assertEqual(True, self.isIdentical_itr(self.build_tree1(),self.build_tree1()))
-
-    def test_2_1(self):
-        self.assertEqual(False, self.isIdentical_itr(self.build_tree1(),self.build_tree2()))
+        self.assertEqual(4, self.findBottomLeftValue(self.build_tree1()))
 
 
     def build_tree1(self):
@@ -62,14 +37,5 @@ class Solution(test_class.test_class):
         tree.right = nd.node(3)
 
         tree.left.left = nd.node(4)
-        tree.left.right = nd.node(5)
-        return tree
-
-    def build_tree2(self):
-        tree = nd.node(1)
-        tree.left = nd.node(2)
-        tree.right = nd.node(3)
-
-        tree.left.left = nd.node(6)
         tree.left.right = nd.node(5)
         return tree
--- Helpers/test_class.py
+++ Helpers/test_class.py
@@ -1,13 +1,28 @@
 import unittest
 from Helpers import profiler as prof
+import yappi
 
 class test_class(unittest.TestCase):
 
     def setUp(self):
         self.prof_start = prof.start()
+        yappi.start(builtins=False)
 
     def tearDown(self):
         prof.stop(self.id(), self.prof_start)
+        #if self.profile:
+        #    self.printProfile(3)
+
+    def printProfile(self, lines):
+        print('\n')
+        stats = yappi.get_func_stats().sort(sort_type='tsub', sort_order='desc')  # .print_all()
+        i = 0
+        print(f'{"function".ljust(30, " ")} {"calls".ljust(10, " ")} time')
+        for stat in stats:
+            print(f'{stat[0].ljust(30," ")} {str(stat[3]).ljust(10," ")} {round(stat[7],3)}')
+            i += 1
+            if i == lines:
+                break
 
 def run():
     unittest.main()
