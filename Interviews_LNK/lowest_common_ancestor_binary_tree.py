fd31a3c85a9f1d3fe3dc7aae8671c5da7d51abed
ramy.saleh <ramy.saleh.me@gmail.com>
2020-02-09 13:27:08 +0000

    Lowest Common Ancestor of a Binary Tree

---
+++ Interviews_LNK/lowest_common_ancestor_binary_tree.py
@@ -0,0 +1,38 @@
+#https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+
+from Helpers import helper as hlp
+from Helpers import test_class
+from Helpers import node as nd
+
+class Solution(test_class.test_class):
+
+    def setUp(self):
+        super().setUp()
+        self.ans = None
+
+    def lowestCommonAncestor(self, root, p, q):
+        self.dfs(root, p, q)
+        return self.ans
+
+    def dfs(self, current_node, p, q):
+        if not current_node:
+            return False
+
+        left = self.dfs(current_node.left, p, q)
+        right = self.dfs(current_node.right, p, q)
+
+        # If the current node is one of p or q
+        mid = current_node.val == p.val or current_node.val == q.val
+
+        if mid and left or mid and right or left and right:
+            self.ans = current_node.val
+
+        # Return True if either of the three bool values is True.
+        return mid or left or right
+
+    def test_1(self):
+        self.assertEqual(3, self.lowestCommonAncestor(hlp.array_to_tree([3,5,1,6,2,0,8,None,None,7,4]),nd.node(5), nd.node(1)))
+
+    def test_2(self):
+        self.assertEqual(5, self.lowestCommonAncestor(hlp.array_to_tree([3,5,1,6,2,0,8,None,None,7,4]),
+                                                          nd.node(5), nd.node(4)))
