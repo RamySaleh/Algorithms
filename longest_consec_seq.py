4ae5927db5661f7a627425a7bbbebe7d4f95d1a5
ramy.saleh <ramy.saleh.me@gmail.com>
2020-02-14 21:01:26 +0000

    longest conseq seq

--- longest_consec_seq.py
+++ longest_consec_seq.py
@@ -10,6 +10,25 @@ class Solution(test_class.test_class):
     def setUp(self):
         super().setUp()
 
+    def longestConsecutive_sort(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        res = 0
+        counter = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                continue
+            elif nums[i] - 1 == nums[i - 1]:
+                counter += 1
+            else:
+                res = max(res, counter)
+                counter = 1
+
+        return max(res, counter)
+
     def longestConsecutive(self, nums: List[int]) -> int:
         num_set = set(nums)
 
@@ -34,6 +53,9 @@ class Solution(test_class.test_class):
 
     def test_3(self):
         self.assertEqual(2, self.longestConsecutive([4, 4]))
+
+    def test_5(self):
+        self.assertEqual(2, self.longestConsecutive([0, -1]))
 
     def test_4(self):
         self.assertEqual(2, self.longestConsecutive([-8, -4, 9, 9, 4, 6, 1, -4, -1, 6, 8]))
