a0b221dfdcb27e109405fe03842982eab735fa50
ramy.saleh <ramy.saleh.me@gmail.com>
2020-03-11 11:32:11 +0000

    Nested List Weight Sum II

--- Interviews_LNK/Nested_List_Weight_Sum.py
+++ Interviews_LNK/Nested_List_Weight_Sum.py
@@ -32,9 +32,37 @@ class Solution(test_class.test_class):
                     stack.append((item, depth + 1))
         return res
 
+    def depthSumInverse(self, nestedList):
+        res = 0
+        self.m_depth = 0
+        self.getMaxDepth(nestedList)
+        stack = [(nestedList, self.m_depth)]
+        while stack:
+            nestedList, depth = stack.pop()
+            for item in nestedList:
+                if isinstance(item, int):
+                    res += item * depth
+                else:
+                    stack.append((item, depth - 1))
+        return res
+
+    def getMaxDepth(self, nestedList, depth=1):
+        self.m_depth = max(depth, self.m_depth)
+        for item in nestedList:
+            if not isinstance(item, int):
+                self.getMaxDepth(item, depth + 1)
 
     def test_1(self):
         self.assertEqual(10, self.depthSum([[1, 1], 2, [1, 1]]))
 
     def test_2(self):
         self.assertEqual(12, self.depthSum([2, [1, 1], [1, 2]]))
+
+    def test_inv_1(self):
+        self.assertEqual(8, self.depthSumInverse([[1, 1], 2, [1, 1]]))
+
+    def test_inv_2(self):
+        self.assertEqual(17, self.depthSumInverse([1,[4,[6]]]))
+
+    def test_inv_3(self):
+        self.assertEqual(-3, self.depthSumInverse([[-1],[[-1]]]))
