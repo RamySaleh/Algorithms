403c4610090169efed47bae48af8c976a18d7bc7
ramy.saleh <ramy.saleh.me@gmail.com>
2020-04-10 13:39:03 +0100

    Maximum Length of a Concatenated String with Unique Characters

--- Interview_MSFT/Max_length_conc_string.py
+++ Interview_MSFT/Max_length_conc_string.py
@@ -13,19 +13,20 @@ class Solution(test_class.test_class):
     def maxLength(self, arr: List[str]) -> int:
         if len(arr) == 1:
             return len(arr[0])
-
-        res = 0
-        dic = {}
+        self.res = 0
+        self.perms = []
         for i in range(len(arr)):
             if self.valid(arr[i]):
-                curr_str = arr[i]
-            else:
-                curr_str = ''
-            for j in range(i, len(arr)):
-                if self.valid(curr_str + arr[j]):
-                    curr_str += arr[j]
-                res = max(res, len(curr_str))
-        return res
+                self.dfs(arr, arr[i], i + 1)
+
+        return self.res
+
+    def dfs(self, arr, str, i):
+        self.perms.append(str)
+        self.res = max(self.res, len(str))
+        for j in range(i, len(arr)):
+            if self.valid(str + arr[j]):
+                self.dfs(arr, str + arr[j], j + 1)
 
     def valid(self, str):
         dic = {}
@@ -36,37 +37,6 @@ class Solution(test_class.test_class):
                 dic[c] = ''
         return True
 
-    def valid2(self, w1, w2):
-        for c in w1:
-            if c in w2:
-                return False
-        return True
-
-    def maxLength2(self, arr: List[str]) -> int:
-        l = 0
-        cur_sum = 0
-        res = 0
-        dic = {}
-        for r in range(len(arr)):
-            cur_sum += len(arr[r])
-            valid = self.isValid(arr[r], dic)
-            if not valid:
-                cur_sum -= len(arr[l])
-                for c in arr[l]:
-                    if c in dic: del dic[c]
-                l += 1
-
-            res = max(res, cur_sum)
-            for c in arr[r]:
-                dic[c] = ''
-
-        return res
-
-    def isValid(self, word, dic):
-        for c in word:
-            if c in dic:
-                return False
-        return True
 
     def test_1(self):
         self.assertEqual(4 ,self.maxLength(["un","iq","ue"]))
@@ -79,6 +49,9 @@ class Solution(test_class.test_class):
 
     def test_4(self):
         self.assertEqual(6 ,self.maxLength(["a", "abc", "d", "de", "def"]))
+
+    def test_12(self):
+        self.assertEqual(16 ,self.maxLength(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]))
 
     def test_13(self):
         self.assertEqual(26 ,self.maxLength(["abcdefghijklm","bcdefghijklmn","cdefghijklmno","defghijklmnop","efghijklmnopq","fghijklmnopqr","ghijklmnopqrs","hijklmnopqrst","ijklmnopqrstu","jklmnopqrstuv","klmnopqrstuvw","lmnopqrstuvwx","mnopqrstuvwxy","nopqrstuvwxyz","opqrstuvwxyza","pqrstuvwxyzab"]))
